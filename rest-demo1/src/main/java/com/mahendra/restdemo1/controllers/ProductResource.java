68e324e92f9c6842cbcb21248aee2bf37e152ba6
Mahendra Shinde <MahendraShinde@synergetics-india.com>
2022-01-24 15:27:09 +0530

    PUT Method

--- rest-demo1/src/main/java/com/mahendra/restdemo1/controllers/ProductResource.java
+++ rest-demo1/src/main/java/com/mahendra/restdemo1/controllers/ProductResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -73,4 +74,18 @@ public class ProductResource {
 		return new ResponseEntity<String>("Product "+id+" deleted successfuly",HttpStatus.OK);
 	}
 	
+	@PutMapping(value="/{id}",consumes="application/json")
+	public ResponseEntity<Product> update(@PathVariable("id") Integer id, @RequestBody Product p){
+		System.out.println("Updating product id "+id);
+		Product temp = findById(id);
+		if(temp==null) {
+			System.out.println("Not found!");
+			return new ResponseEntity<Product>(HttpStatus.NOT_FOUND);
+		}
+		else {
+		temp.setName(p.getName());
+		temp.setPrice(p.getPrice());
+		return new ResponseEntity<Product>(temp,HttpStatus.OK);
+		}
+	}
 }
